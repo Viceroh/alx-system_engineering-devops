4cccac21c4677f567d48856ccdc9964ea9c71171
Viceroh <rotichb105@gmail.com>
2024-05-09 21:52:30 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""How many subs?"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    get number of subscribers
-    args:
-        subreddit: the name of subreddit
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url=url)
-    if (response.status_code != 200):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    subscribers = response.json()["data"]["subscribers"]
-    return (subscribers)
